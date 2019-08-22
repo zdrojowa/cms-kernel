a5f62c4c02d0acfa0bb8a45d1c33123fc3ab5868
Gutek <g.bzinkowski@zdrojowainvest.pl>
2019-08-22 13:55:38 +0200

    Add menu repository value to core modules enum class

--- src/Utils/Enums/CoreModulesEnum.php
+++ src/Utils/Enums/CoreModulesEnum.php
@@ -18,6 +18,7 @@ class CoreModulesEnum extends Enum
     public const BOOTER = 'cms-booter';
     public const MODULE_MANAGER = 'cms-module-manager';
     public const ACL_REPOSITORY = 'cms-acl-repository';
+    public const MENU_REPOSITORY = 'cms-menu-repository';
     public const MIGRATION_REPOSITORY = 'cms-migration-repository';
     public const MIGRATOR = 'cms-migrator';
 }
