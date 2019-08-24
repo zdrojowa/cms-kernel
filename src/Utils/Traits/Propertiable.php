9dcc80b400dbd8710982469ba2a03c04ed1bc221
Jakub Kosiński <jakub.kosinski.99@gmail.com>
2019-08-24 22:29:21 +0200

    Refactor and comments

--- src/Utils/Traits/Propertiable.php
+++ src/Utils/Traits/Propertiable.php
@@ -3,14 +3,15 @@
 namespace Zdrojowa\CmsKernel\Utils\Traits;
 
 use Exception;
-use Validator;
+use Illuminate\Support\Facades\Validator;
+use Zdrojowa\CmsKernel\Exceptions\CmsKernelException;
 use Zdrojowa\CmsKernel\Exceptions\PropertyBinder\PropertyCanNotPassValidationException;
 use Zdrojowa\CmsKernel\Exceptions\PropertyBinder\PropertyIsRequiredException;
 use Zdrojowa\CmsKernel\Exceptions\PropertyBinder\PropertyNameMustBeAStringException;
-use Zdrojowa\CmsKernel\Utils\Variabler\Variabler;
 
 /**
  * Trait PropertyBinder
+ * This trait is for binding property to classes based on data given
  * @package Zdrojowa\CmsKernel\Utils\Traits
  */
 trait Propertiable
@@ -21,31 +22,50 @@ trait Propertiable
      * @param array $rules
      * @param bool $required
      *
-     * @throws Exception
+     * @throws CmsKernelException
      */
     final private function bindProperties(array $data, array $properties, array $rules = [], bool $required = false)
     {
         foreach ($properties as $property) {
-            if (!is_string($property)) throw new PropertyNameMustBeAStringException($this);
-
-            if (!array_key_exists($property, $data)) {
-                if ($required) {
-                    if (!array_key_exists($property, $data)) throw new PropertyIsRequiredException([$this, $property]);
-                }
-
-                continue;
-            }
-
-            if (array_key_exists($property, $rules)) {
-                $validator = Validator::make([$property => $data[$property]], $rules);
-
-                if ($validator->fails()) throw new PropertyCanNotPassValidationException([
-                    $property,
-                    $rules[$property],
-                ]);
-            }
+            if (!$this->validateBinder($property, $data, $rules, $required)) continue;
 
             $this->$property = $data[$property];
         }
+    }
+
+    /**
+     * @param string $property
+     * @param array $data
+     * @param array $rules
+     * @param bool $required
+     *
+     * @return bool
+     * @throws CmsKernelException
+     */
+    final private function validateBinder(string $property, array $data, array $rules = [], bool $required = false): bool
+    {
+        // Check if property is valid string
+        if (!is_string($property)) throw new PropertyNameMustBeAStringException($this);
+
+        // Check if property is required and is not empty
+        if (!array_key_exists($property, $data)) {
+            if ($required) {
+                if (!array_key_exists($property, $data)) throw new PropertyIsRequiredException([$this, $property]);
+            }
+
+            return false;
+        }
+
+        // Validate property based on Laravel Validation Rules in $rules variable
+        if (array_key_exists($property, $rules)) {
+            $validator = Validator::make([$property => $data[$property]], $rules);
+
+            if ($validator->fails()) throw new PropertyCanNotPassValidationException([
+                $property,
+                $rules[$property],
+            ]);
+        }
+
+        return true;
     }
 }
