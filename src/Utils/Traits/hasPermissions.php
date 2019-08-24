b09e19708cacb7ae1834e4761a042695cb7a6bba
Jakub Kosiński <jakub.kosinski.99@gmail.com>
2019-08-24 22:34:47 +0200

    hasPermissions comments

--- src/Utils/Traits/hasPermissions.php
+++ src/Utils/Traits/hasPermissions.php
@@ -7,12 +7,14 @@ use Zdrojowa\CmsKernel\Utils\Enums\CoreEnum;
 
 /**
  * Trait hasPermissions
+ * Trait for using in authenticatable models to check permissions based on table which can be configured in config file
  * @package Zdrojowa\CmsKernel\Utils\Traits
  */
 trait hasPermissions
 {
 
     /**
+     * Get model permission package
      * @return mixed
      */
     public function permissions()
@@ -21,6 +23,7 @@ trait hasPermissions
     }
 
     /**
+     * Check if model permission package has given anchor
      * @param string $permission
      *
      * @return bool
@@ -33,6 +36,7 @@ trait hasPermissions
     }
 
     /**
+     * Return bool from model's table
      * @return bool
      */
     public function isAdmin(): bool
